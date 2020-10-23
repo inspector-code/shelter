d6265307380328935efcd7f8269c159d6008e23d
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-10-24 00:38:19 +0300

    feat: add popap on items

--- shelter/assets/js/script.js
+++ shelter/assets/js/script.js
@@ -96,7 +96,6 @@ async function getPets() {
     }
 
     const randomArr = sort863(fullPetsList)
-    setPets(randomArr)
     return randomArr
 }
 
@@ -106,29 +105,111 @@ function setPets(data) {
     data.forEach(i => {
         const containerItem = document.createElement('div')
         containerItem.classList.add('our-friends__content__slider__container-item')
+
         const itemImageContainer = document.createElement('div')
         itemImageContainer.classList.add('our-friends__content__slider__container-item-image')
+
         const itemImage = document.createElement('img')
         itemImage.src = i.img
         itemImage.alt = i.name
         itemImageContainer.append(itemImage)
         containerItem.append(itemImageContainer)
+
         const itemName = document.createElement('div')
         itemName.classList.add('our-friends__content__slider__container-item-title')
         itemName.innerText = i.name
         containerItem.append(itemName)
+
         const itemButton = document.createElement('a')
         itemButton.classList.add('our-friends__content__slider__container-item-button')
         itemButton.innerText = 'Learn more'
-        itemButton.href = '#'
         containerItem.append(itemButton)
 
-        fragment.append(containerItem)
+        containerItem.addEventListener('click', () => {
+            const popup = document.createElement('div')
+            popup.classList.add('popup')
+            popup.id = 'popup'
+            popup.addEventListener('click', () => {
+                popup.remove()
+            })
+
+            const popupContent = document.createElement('div')
+            popupContent.classList.add('popup__content')
+            popupContent.addEventListener('click', (e) => {
+                e.stopPropagation()
+            })
+            popup.append(popupContent)
+
+            const closeButton = document.createElement('button')
+            closeButton.classList.add('popup__content__button')
+            closeButton.id = 'popup__close__button'
+            closeButton.addEventListener('click', () => {
+                popup.remove()
+            })
+            popupContent.append(closeButton)
+
+            const popupImg = document.createElement('div')
+            popupImg.classList.add('popup__content__img')
+
+            const popupImgPet = document.createElement('img')
+            popupImgPet.src = i.img
+            popupImgPet.alt = i.name
+            popupImg.append(popupImgPet)
+            popupContent.append(popupImg)
+
+            const popupContentInfo = document.createElement('div')
+            popupContentInfo.classList.add('popup__content__info')
+            popupContent.append(popupContentInfo)
+
+            const popupTitle = document.createElement('div')
+            popupTitle.classList.add('popup__content__info-title')
+            popupTitle.innerText = i.name
+            popupContentInfo.append(popupTitle)
+
+            const popupSubtitle = document.createElement('div')
+            popupSubtitle.classList.add('popup__content__info-subtitle')
+            popupSubtitle.innerHTML = `${i.type} - ${i.breed}`
+            popupContentInfo.append(popupSubtitle)
+
+            const popupText = document.createElement('div')
+            popupText.classList.add('popup__content__info-text')
+            popupText.innerText = i.description
+            popupContentInfo.append(popupText)
+
+            const popupAbout = document.createElement('div')
+            popupAbout.classList.add('popup__content__info-about')
+            popupContentInfo.append(popupAbout)
+
+            const popupList = document.createElement('ul')
+            popupList.classList.add('popup__content__info-about-list')
+
+            const popupListAge = document.createElement('li')
+            popupListAge.innerHTML = `<span><b>Age:</b> ${i.age}</span>`
+            popupList.append(popupListAge)
+
+            const popupInoc = document.createElement('li')
+            popupInoc.innerHTML = `<span><b>Inoculations:</b> ${i.inoculations.join(', ')}</span>`
+            popupList.append(popupInoc)
+
+            const popupDis = document.createElement('li')
+            popupDis.innerHTML = `<span><b>Diseases:</b> ${i.diseases.join(', ')}</span>`
+            popupList.append(popupDis)
+
+            const popupParasit = document.createElement('li')
+            popupParasit.innerHTML = `<span><b>Parasites:</b> ${i.parasites.join(', ')}</span>`
+            popupList.append(popupParasit)
+
+            popupAbout.append(popupList)
+
+            document.body.append(popup)
+        })
+
+        fragment.prepend(containerItem)
     })
     sliderTrack.append(fragment)
 }
 
-getPets().then((res) => {
+function initSlider(res) {
     const sliderContainer = document.querySelector('.our-friends__content__slider__container')
     const prevBtn = document.getElementById('slider-prev')
     const nextBtn = document.getElementById('slider-next')
@@ -180,4 +261,9 @@ getPets().then((res) => {
         initValues()
         trackIt(slideNumber)
     })
+}
+
+getPets().then(data => {
+    setPets(data)
+    initSlider(data)
 })
